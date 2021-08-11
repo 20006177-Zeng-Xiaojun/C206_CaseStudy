170058aabe8461dcf008455a0392a11b9b85b740
user <user@FIZZZ-FZ10>
2021-08-11 10:05:00 +0800

    Arraylist

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class C206_CaseStudy {
-	
+
+	ArrayList<FoodItem> foodItemList = new ArrayList<FoodItem>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- C206_CaseStudy/src/FoodItem.java
+++ C206_CaseStudy/src/FoodItem.java
@@ -10,4 +10,23 @@
 
 public class FoodItem {
 
+	private String name;
+	private int price;
+	
+	public FoodItem(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+	
+	
 }
