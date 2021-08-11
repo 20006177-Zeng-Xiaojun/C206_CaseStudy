11862a8294c254600f8ccfa8e9322462983fcbaa
user <user@FIZZZ-FZ10>
2021-08-11 09:46:11 +0800

    FoodItem class

---
+++ C206_CaseStudy/src/FoodItem.java
@@ -0,0 +1,13 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Student Name: Hafiz 
+ * Student ID: 20018635
+ * Date/Time created: Wednesday 11-44-2021 09:44
+ */
+
+public class FoodItem {
+
+}
