1f01a76a0f25ba4ccfdd8251be99073ac5d822a6
20006177-Zeng-Xiaojun <87606184+20006177-Zeng-Xiaojun@users.noreply.github.com>
2021-08-11 10:58:18 +0800

    Update C206_CaseStudyTest.java

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -8,76 +8,13 @@ import org.junit.Test;
 
 public class C206_CaseStudyTest {
 	
-	private PurchaseOrder po1;
-	private PurchaseOrder po2;
-	private PurchaseOrder po3;
-
-	private ArrayList<PurchaseOrder> poList;
-	
 	@Before
 	public void setUp() throws Exception {
-		po1 = new PurchaseOrder (1,5,"Fruit Stall");
-		po2 = new PurchaseOrder (2,8,"Drink Stall");
-		po3 = new PurchaseOrder (3,4,"Mix Veg Stall");
-	
-		poList= new ArrayList<PurchaseOrder>();
+		
 	}
 
-	@Test
-	public void testAddPurchaseOrder() {
-		poList.add(po1);
-		//Test that purchase order list is empty, after add 1 purchase order, the size of the list is 1. (Normal condition)
-		assertEquals(1,poList.size());
-		
-		//Test that purchase order list didn’t not meet the maximum order, so that a new item can be added to (Boundary condition)
-		assertNotEquals(8, poList.size());
-		poList.add(po2);
-		
-		//Test that purchase order that newly added is in the same item as the last purchase order in the list, if not flag out error (Error condition)
-		assertSame(po2, poList.get(poList.size()-1));
-	}
-	
-	@Test
-	public void testRetrieveAllPO() {
-		//Test that purchase order list is not null, so that the string output will display the list of items (Boundary condition)
-		assertNotNull(poList);
-		C206_CaseStudy.retrieveAllPO(poList);
-		C206_CaseStudy.viewAllPO(poList);
-		
-		//Test that given list and the retrieved list output is the same (Normal Condition)
-		poList.add(po1);
-		String outputString = String.format("%-10s %-30s %-10s\n", "1", "5","Fruit Stall");
-		assertEquals(outputString, C206_CaseStudy.retrieveAllPO(poList));
-		
-		//Test that given list is empty, after adding 2 items, the size list is 2, if not flag out error. (Error condition)
-		poList.add(po2);
-		assertEquals(2, poList.size());
-	}
-	
-	@Test
-	public void testDeletePO() {
-		poList.add(po1);
-		poList.add(po2);
-		poList.add(po3);
-		//Purchase order to be deleted is currently available. If not available, flag out error. (Error condition)
-		assertTrue(C206_CaseStudy.findPO(poList,po1.getPoID()));
-		poList.remove(po1);
-		
-		//Test that purchase order deleted is not longer in the in the list. (Normal condition)
-		assertFalse(C206_CaseStudy.findPO(poList,po1.getPoID()));
-		
-		//Test that purchase order list is not null, so to retrieved the purchase order and delete it. (Boundary condition)
-		assertNotNull(poList);
-		C206_CaseStudy.retrieveAllPO(poList);
-		poList.remove(po2);
-	}
-	
-	
 	@After
 	public void tearDown() throws Exception {
-		po1 = null;
-		po2 = null;
-		po3 = null;
-		poList = null;
+		
 	}
 }
